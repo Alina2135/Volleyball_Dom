b4f10beef22d4f3d0c6d23c387e8eaf22290e357
Alina2135 <166717651+Alina2135@users.noreply.github.com>
2025-11-18 23:49:56 +0200

    Update articles_model.js

--- model/articles_model.js
+++ model/articles_model.js
@@ -1,12 +1,20 @@
 export class ArticlesModel {
-    async getAllArticles() {
-        const response = await fetch("https://volleyball-mvc-server-2.onrender.com/api/articles");
-        const articles = await response.json();
-        return articles;
+    constructor() {
+        this.articles = [
+            { id: 1, title: "Вступ до MVC", author: "Бірюк Аліна", year: 2025,
+              content: "Ця стаття пояснює основи архітектури MVC та її застосування у веб-розробці." },
+            { id: 2, title: "Основи JavaScript", author: "Петренко Ігор", year: 2024,
+              content: "Основні концепції JavaScript, включаючи змінні, функції та роботу з DOM." },
+            { id: 3, title: "Frontend у 2025 році", author: "Бірюк Аліна", year: 2025,
+              content: "Огляд тенденцій у фронтенд розробці, нові технології та інструменти." }
+        ];
     }
 
-    async getArticlesByAuthor(author) {
-        const allArticles = await this.getAllArticles();
-        return allArticles.filter(a => a.author === author);
+    getAllArticles() {
+        return this.articles;
+    }
+
+    getArticlesByAuthor(author) {
+        return this.articles.filter(a => a.author === author);
     }
 }
